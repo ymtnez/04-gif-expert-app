e87c115188e9d067caee9b6852070cfbc508c4c5
Yohandys Martinez <ymtnez@upr.edu.cu>
2023-04-03 17:22:56 -0400

    Actualizando index.js por el tema de promesas

--- src/index.js
+++ src/index.js
@@ -7,29 +7,25 @@ import './index.css';
 
 ReactDOM.render(
   <GifExpertApp />,
-  document.getElementById('root')  
+  document.getElementById('root')
 );
 
-const getGifs = () => {  
+const getGifs = async() => {
 
-  //https://api.giphy.com/v1/gifs/random?api_key=
+   const apiKey = 'd718ghUvwIvoMBlizpYESBSR5nHhr77w';   
+   const urlApi = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`;
+   const peticion = await fetch(urlApi);
 
-  //const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=Ns1IgnDyFXIpSxgiZSOj9RTSj4vDOCt3';
-  const url = 'https://api.giphy.com/v1/gifs/random?api_key=83i7O10QCBh6UcA8OfZ2Bn9UJPR4f356';
-  const peticion =  fetch(url);
+   const resp = peticion.json();
+   resp.then( ({data}) => {
+      const url = data.images.original.url;
+      
+      const imagen = document.createElement('img');
+      imagen.src = url;
+      document.body.append(imagen);
+   })
 
-  peticion
-    .then( (resp) => resp.json() )//En este caso hacemos un return implicito
-    .then( ({data}) => {
-        const {url} = data.images.original;
-
-        const imagen = document.createElement('img');
-        imagen.src   = url;
-        document.body.append(imagen); 
-
-        console.log(data)
-    })
-
+   
 
 
 };
