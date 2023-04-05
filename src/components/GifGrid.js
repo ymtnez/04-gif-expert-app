0580493991f54230a4b9aaf130d2fe3a62910c98
Yohandys <ymtnez@upr.edu.cu>
2023-04-05 15:13:51 -0500

    Creando el GifGridItem y modificando estilos

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GifGridItem } from './GifGridItem';
 
-export const GifGrid = ({ categ }) => {
+export const GifGrid = ({ categ }) => {    
+    
+    /**
+     * Si no utilizamos useEffect, entonces por ejemplo en el caso que queramos
+     * utilizar el hook de useState para cambiar el counter mediante el evento
+     * onClick del boton se va ejecutar todo la peticion http, o lo que es lo 
+     * mismo, React detecta un cambio en el componente (cambio el counter) y
+     * vuelve a ejecutar todo el codigo.
+            const [counter, setCounter] = useState(0);
+            .
+            .
+            .
+            return (
+                <div>
+                    <h1>{ counter }</h1>
+                    <button onClick={ () => setCounter( counter + 1 ) }>Click</button>
+                    <h3>{ categ }</h3>  
+                </div>
+    )
+    */
+    
+    //Utilizando el hook useState
+    const [images, setImages] = useState([]);
+    
+   /**
+    * Aqui estamos utilizando otro hook (useEffect) para indicarle a React
+    * que ejecute getGifs() solo una vez aun si detecta un cambio. Y solo
+    * una vez pues en el segundo argumento pasamos el array vacio. 
+    */
+    useEffect(() => {
+        getGifs();
+    }, []);
 
     const getGifs = async() => {
 
@@ -30,15 +62,43 @@ export const GifGrid = ({ categ }) => {
             }    
         });
 
-        console.log(gifs);
+        setImages( gifs );
     }
-
-   getGifs();
-
-
+   
     return (
-        <div>
-            <h3>{ categ }</h3>  
-        </div>
+        <>
+            <h3>{ categ }</h3>
+            <div className='card-grid'>
+                {/* <ol>
+                    {
+                        //
+                        // Pudiera ser 
+                        //    images.map( elem => <li key={ elem.id }>{ elem.title }</li> )
+                        // pero el array images es un arreglo de objetos donde sus artibutos
+                        // son id, title y url por lo que si desestructuramos quedaria mas
+                        // limpio el codigo. 
+                            
+                        images.map( ({ id, title }) => (
+                            <li key={ id }>{ title }</li>
+                        ))
+                    }
+                </ol> */}
+                {
+                    images.map( (imagen) => (
+                        <GifGridItem
+                            key={imagen.id}
+                            /**
+                             * Imagen es un objeto y utilizando el operador spread
+                             * podemos hacer {...imagen}, esto nos permite enviar
+                             * como properties independientes a cada uno de los 
+                             * atributos, campos o propiedades hacia el componente
+                             * que lo reciba.
+                             */
+                            { ...imagen }
+                        />
+                    ))   
+                } 
+            </div>        
+        </>
     )
 }
